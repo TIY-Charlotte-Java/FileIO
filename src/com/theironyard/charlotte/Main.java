4b106c7840c7ce12e62f16a58022c68b25d40102
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-09 13:38:02 -0500

    initial attempt

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,44 @@
 package com.theironyard.charlotte;
 
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        MasterChef chef = new MasterChef();
+        chef.bakery();
+        File f = new File("service.json");
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.serialize(chef);
+        FileWriter fw = new FileWriter(f);
+        fw.write(json);
+        fw.close();
+
+        Scanner scanner = new Scanner(f);
+        scanner.useDelimiter("\\Z");
+        String content = scanner.next();
+        JsonParser parser = new JsonParser();
+        MasterChef baker2 = parser.parse(content,MasterChef.class);
+
+        System.out.println(baker2);
+
+
+
     }
+
 }
+
+
+
+
+
+
+
