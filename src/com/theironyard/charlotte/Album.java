6961d10359941abdcb2a6dce714fc6b7b9d57fe4
Kelsey Newman <kpnewman2013@gmail.com>
2016-12-23 18:02:26 -0500

    saving what I've done so far

---
+++ src/com/theironyard/charlotte/Album.java
@@ -0,0 +1,85 @@
+package com.theironyard.charlotte;
+
+
+import java.util.Scanner;
+
+/**
+ * Created by kelseynewman on 12/9/16.
+ */
+public class Album {
+    private String albumName;
+    private String artistName;
+    private String favoriteSong;
+    private String whatFeels;
+    private String whereHeard;
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getFavoriteSong() {
+        return favoriteSong;
+    }
+
+    public void setFavoriteSong(String favoriteSong) {
+        this.favoriteSong = favoriteSong;
+    }
+
+    public String getWhatFeels() {
+        return whatFeels;
+    }
+
+    public void setWhatFeels(String whatFeels) {
+        this.whatFeels = whatFeels;
+    }
+
+    public String getWhereHeard() {
+        return whereHeard;
+    }
+
+    public void setWhereHeard(String whereHeard) {
+        this.whereHeard = whereHeard;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    public void album() {
+
+        System.out.println("What is the name of the album?");
+        setAlbumName(scanner.nextLine());
+
+        System.out.println("What is the name of the artist that released the album?");
+        setArtistName(scanner.nextLine());
+
+        System.out.println("what is your favorite song on the album?");
+        setFavoriteSong(scanner.nextLine());
+
+        System.out.println("What kind of feels do you get from this album?");
+        setWhatFeels(scanner.nextLine());
+
+        System.out.println("Where/when did you first hear the song that led you to get the album?");
+        setWhereHeard(scanner.nextLine());
+    }
+
+    @Override
+    public String toString() {
+        return  "Album name: " + getAlbumName() +  "\n "
+                + "Artist name: " + getArtistName() +  "\n "
+                + "Favorite song: " + getFavoriteSong() +  "\n "
+                + "What feels: " + getWhatFeels() +  "\n "
+                + "Where heard: " + getWhereHeard();
+    }
+}
--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,42 @@
 package com.theironyard.charlotte;
 
-public class Main {
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class Main {
+    static Album a = new Album();
+    public static JsonSerializer serializer = new JsonSerializer();
+    public static JsonParser parser = new JsonParser();
+
+    public static void main(String[] args) throws IOException{
+        a.album();
+        File album = new File("album.json");
+        FileWriter fw = new FileWriter(album);
+
+        //writes to the JSON file
+        String albumInfo = serializer.serialize(Main.a);
+        fw.write(albumInfo);
+        fw.close();
+
+        System.out.println("Would you like to change any of this information?");
+        Scanner scanner = new Scanner(album);
+        String contents = scanner.next();
+        scanner.useDelimiter("\\Z");
+        parser.parse(contents, Album.class);
+        Scanner responseScanner = new Scanner(System.in);
+        String input  = scanner.nextLine();
+        if (input.equals("yes")) {
+            fw.append(responseScanner.nextLine());
+        } else if (input.equals("no")) {
+            System.exit(0);
+        }
     }
+
+
 }
